0ad011676b60e2ed1b4de57e8eda45eed4b6907f
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-02-07 21:01:44 +0600

    Time: 9 ms (24.09%), Space: 9.2 MB (78.22%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        int tmp1;
+        
+        string answer = "" ;
+        
+        int smallest = 1000 ;
+        
+        for( int i = 0 ; i < strs.size() ; i++ ){
+            tmp1 = strs[i].size();
+            smallest = min( smallest , tmp1 ) ;
+        }
+        
+        bool flag = false ;
+        
+        for( int i = 0 ; i < smallest ; i++ ){
+            
+            for( int j = 0 ; j < strs.size() ; j++ ){
+                
+                if( strs[0][i] != strs[j][i] ){
+                    flag = true ;
+                    break;
+                }
+                
+                if( j == strs.size() - 1 ){
+                    answer.push_back(strs[0][i]);
+                }
+                
+            }
+            
+            if(flag)break;
+            
+        }
+        
+        return answer;
+        
+    }
+};
