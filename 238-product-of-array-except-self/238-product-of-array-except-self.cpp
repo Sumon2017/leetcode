915e259028aeeb513c9d09ef4ae2fed46a9fe741
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-03-04 23:43:41 +0600

    Update 238-product-of-array-except-self.cpp

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,36 +2,25 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        int prod = 1 ;
-        
-        int zero = 0 ;
-        
-        for (int i = 0 ; i < nums.size() ; i++){
-            
-            if(nums[i] == 0){
-                zero ++ ;
-            }
-            else{
-                prod = prod * nums[i] ;
-            }
-            
+        vector<int>pre_prod;
+        vector<int>post_prod;
+        int cur_prod = 1;
+        for(int i = 0 ; i < nums.size() ; i++){
+            cur_prod = cur_prod * nums[i];
+            pre_prod.push_back(cur_prod);
         }
-        
+        cur_prod = 1;
+        for(int i = nums.size()-1 ; i>=0 ; i--){
+            cur_prod = cur_prod * nums[i];
+            post_prod.push_back(cur_prod);
+        }
+        reverse(post_prod.begin(),post_prod.end());
         vector<int>answer;
-        
-        for (int i = 0 ; i < nums.size() ; i++){
-            
-            if(nums[i] == 0){
-                if(zero > 1 )answer.push_back(0);
-                else answer.push_back(prod);
-            }
-            else{
-                if(zero > 0)answer.push_back(0);
-                else answer.push_back(prod/nums[i]);
-            }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i==0)answer.push_back(post_prod[i+1]);
+            else if(i==nums.size()-1)answer.push_back(pre_prod[i-1]);
+            else answer.push_back(pre_prod[i-1]*post_prod[i+1]);
         }
-        
         return answer;
-        
     }
 };
