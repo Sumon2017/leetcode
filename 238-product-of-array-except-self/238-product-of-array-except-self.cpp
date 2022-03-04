d0a52258683869521bf8c736ee76a29991da605b
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-03-05 00:03:20 +0600

    Update 238-product-of-array-except-self.cpp

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        vector<int>pre_prod;
-        vector<int>post_prod;
+        vector<int>answer(nums.size(),1);
         int cur_prod = 1;
-        for(int i = 0 ; i < nums.size() ; i++){
-            cur_prod = cur_prod * nums[i];
-            pre_prod.push_back(cur_prod);
+        for(int i = 0 ; i < nums.size() - 1 ; i++){
+            cur_prod = cur_prod*nums[i];
+            answer[i+1]=cur_prod;
         }
         cur_prod = 1;
-        for(int i = nums.size()-1 ; i>=0 ; i--){
-            cur_prod = cur_prod * nums[i];
-            post_prod.push_back(cur_prod);
-        }
-        reverse(post_prod.begin(),post_prod.end());
-        vector<int>answer;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(i==0)answer.push_back(post_prod[i+1]);
-            else if(i==nums.size()-1)answer.push_back(pre_prod[i-1]);
-            else answer.push_back(pre_prod[i-1]*post_prod[i+1]);
+        for(int i = nums.size() - 1 ; i > 0 ; i--){
+            cur_prod = cur_prod*nums[i];
+            answer[i-1] = answer[i-1]*cur_prod;
         }
         return answer;
     }
