0b76368b752b729fe2ecf9d191e6a026ba71d7b9
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-03-04 20:50:35 +0600

    Update 121-best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-        if(prices.size() < 2)return 0;
-        
-        int min_var = 100000 ;
-        vector<int>min_arr;
-        
-        for( int i = 0 ; i < prices.size() ; i++ ){
-            min_var = min(min_var , prices[i]);
-            min_arr.push_back(min_var);
+        if(prices.size()==1)return 0;
+        int l = 0 , r = 1 ;
+        int max_profit = 0 ;
+        while(r<prices.size()){
+            max_profit = max(max_profit,prices[r]-prices[l]);
+            if(prices[r] < prices[l])l=r;
+            r=r+1;
         }
-        
-        int max_var = -1 ;
-        
-        for( int i = 1 ; i < prices.size() ; i++ ){
-            max_var = max(max_var , prices[i] - min_arr[i-1] );
-        }
-        
-        if(max_var < 0)return 0;
-        else return max_var;
+        return max_profit;
     }
 };
