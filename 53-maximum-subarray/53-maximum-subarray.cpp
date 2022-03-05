ff0068df0f0c8a265d77d47bedde61bbf046a462
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-03-05 16:00:49 +0600

    Time: 190 ms (27.27%), Space: 67.8 MB (54.60%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        vector<long>q_sum;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(i == 0)
-                q_sum.push_back(nums[i]);
-            else
-                q_sum.push_back(*q_sum.rbegin() + nums[i]);
+        int l = 0 , r = 0 ;
+        long cur_max = -1e9-1;
+        long cur_sum = 0;
+        while(r<nums.size()){
+            cur_sum = cur_sum + nums[r];
+            cur_max = max(cur_max,cur_sum);
+            if(cur_sum<1){
+                cur_sum=0;
+                l=r+1;
+            }
+            r=r+1;
         }
-        
-        long cur_min = 0;
-        vector<long>min_arr;
-        min_arr.push_back(0);
-        for(int i = 0 ; i < q_sum.size() ; i++){
-            cur_min = min(cur_min , q_sum[i]);
-            min_arr.push_back(cur_min);
-        }
-        
-        long answer = -1e9-1;
-        long temp;
-        for(int i = 0 ; i < q_sum.size() ; i++){
-            temp = q_sum[i] - min_arr[i];
-            answer = max(answer , temp );
-        }
-        return (int)answer;
+        return (int)cur_max;
         
     }
 };
