efbd2abc013024eab973d2602a69960aed221190
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-05-04 00:16:03 +0600

    Time: 821 ms (5.01%), Space: 16.8 MB (99.56%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -2,16 +2,7 @@ class Solution {
     int r,c;
     bool pacific,atlantic;
     int visited[201][201];
-    bool off_flag;
-    int in_answer[201][201];
     void dfs(int i,int j,vector<vector<int> >& grid){
-        if(off_flag==true)return;
-        if(in_answer[i][j]==1){
-            pacific=true;
-            atlantic=true;
-            off_flag=true;
-            return;
-        }
         visited[i][j]=1;
         if(i==0 or j==0)pacific=true;
         if(i==r-1 or j==c-1)atlantic=true;
@@ -25,18 +16,13 @@ public:
         vector<vector<int> >result;
         r = heights.size();
         c = heights[0].size();
-        memset(in_answer,0,sizeof(in_answer));
         for(int i=0 ; i<r ; i++){
             for(int j=0 ; j<c ; j++){
                 pacific=false;
                 atlantic=false;
                 memset(visited,0,sizeof(visited));
-                off_flag=false;
                 dfs(i,j,heights);
-                if(pacific==true and atlantic==true){
-                    result.push_back(vector<int>{i,j});
-                    in_answer[i][j]=1;
-                }
+                if(pacific==true and atlantic==true)result.push_back(vector<int>{i,j});
             }
         }
         return result;
