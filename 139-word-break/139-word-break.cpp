530d02a5b009d82d8415d532f56fcfe26db455b1
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-06-06 00:27:02 +0600

    Time: 37 ms (20.05%), Space: 23.6 MB (5.38%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    class myInt{
+        public:
+        int val;
+        myInt(){
+            val =-1;
+        }
+    };
+    map<string,myInt>mymap;
+    string input;
+    vector<map<string,myInt> >look_up;
+    bool func(int idx=0,string current=""){
+        if(look_up[idx][current].val != -1)return look_up[idx][current].val;
+        if(idx == input.size()){
+            return look_up[idx][current].val = current.size() == 0 ;
+        }
+        
+        current.push_back(input[idx]);
+        
+        if(mymap[current].val==1){
+            return look_up[idx][current].val = (func(idx+1,"") or func(idx+1,current));
+        }
+        
+        return look_up[idx][current].val = func(idx+1,current);
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(int i=0;i<=301;i++)look_up.push_back(map<string,myInt>());
+        input = s;
+        for(auto x:wordDict){
+            mymap[x].val=1;
+        }
+        return func();
+    }
+};
