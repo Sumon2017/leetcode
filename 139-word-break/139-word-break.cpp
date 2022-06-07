a53c1a9b58a8ac61c33d6fc8a087a5d2c278c14e
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-06-08 04:52:22 +0600

    Time: 3 ms (94.82%), Space: 7.4 MB (99.79%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,35 +1,15 @@
 class Solution {
-    class myInt{
-        public:
-        int val;
-        myInt(){
-            val =-1;
-        }
-    };
-    map<string,myInt>mymap;
-    string input;
-    vector<map<string,myInt> >look_up;
-    bool func(int idx=0,string current=""){
-        if(look_up[idx][current].val != -1)return look_up[idx][current].val;
-        if(idx == input.size()){
-            return look_up[idx][current].val = current.size() == 0 ;
-        }
-        
-        current.push_back(input[idx]);
-        
-        if(mymap[current].val==1){
-            return look_up[idx][current].val = (func(idx+1,"") or func(idx+1,current));
-        }
-        
-        return look_up[idx][current].val = func(idx+1,current);
-    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        for(int i=0;i<=301;i++)look_up.push_back(map<string,myInt>());
-        input = s;
-        for(auto x:wordDict){
-            mymap[x].val=1;
+        vector<bool>dp(s.size()+1,false);
+        dp[dp.size()-1]=true;
+        for(int i=s.size()-1 ; i>=0 ; i--){
+            for(auto word:wordDict){
+                if(i+word.size() <= s.size() and s.substr(i,word.size()) == word){
+                    dp[i] = dp[i] | dp[i+word.size()];
+                }
+            }
         }
-        return func();
+        return dp[0];
     }
 };
