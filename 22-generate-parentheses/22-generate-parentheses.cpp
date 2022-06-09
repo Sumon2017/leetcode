171256392045c0ccf0c8827e5932ac1cc115ceff
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-06-09 18:19:03 +0600

    Time: 92 ms (5.02%), Space: 79.4 MB (5.00%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -5,20 +5,26 @@ public:
         int N = 2*n;
         int stop_n = pow(2,N);
         int x = 0;
+        stack<char>mystack;
+        string arr = "";
         while(x<stop_n){
             int current = x ;
-            stack<char>mystack;
-            string arr = "";
+            mystack = stack<char>();
+            arr = "";
+            int a=0,b=0;
             for(int i=0;i<N;i++){
+                (current & 1) ? a++ : b++ ;
                 arr.push_back( (current & 1) ? '(' : ')' );
                 current >>= 1;
+                if(a>n or b>n)break;
             }
+            x++;
+            if(a!=b)continue;
             for(int i=0;i<arr.size();i++){
                 if(!mystack.empty() and mystack.top() == '(' and arr[i] == ')' )mystack.pop();
                 else mystack.push(arr[i]);
             }
             if(mystack.empty())answer.push_back(arr);
-            x++;
         }
         return answer;
     }
