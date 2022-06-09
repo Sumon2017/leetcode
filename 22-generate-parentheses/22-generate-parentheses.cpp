0897edaa11f6a23283558c67022d9578998f570c
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-06-09 18:39:21 +0600

    Time: 121 ms (5.02%), Space: 79.2 MB (5.00%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -4,27 +4,23 @@ public:
         vector<string>answer;
         int N = 2*n;
         int stop_n = pow(2,N);
-        int x = 0;
+        int counter = 0;
         stack<char>mystack;
         string arr = "";
-        while(x<stop_n){
-            int current = x ;
+        while(counter<stop_n){
+            int current = counter ;
             mystack = stack<char>();
             arr = "";
-            int a=0,b=0;
             for(int i=0;i<N;i++){
-                (current & 1) ? a++ : b++ ;
                 arr.push_back( (current & 1) ? '(' : ')' );
                 current >>= 1;
-                if(a>n or b>n)break;
             }
-            x++;
-            if(a!=b)continue;
             for(int i=0;i<arr.size();i++){
                 if(!mystack.empty() and mystack.top() == '(' and arr[i] == ')' )mystack.pop();
                 else mystack.push(arr[i]);
             }
             if(mystack.empty())answer.push_back(arr);
+            counter++;
         }
         return answer;
     }
