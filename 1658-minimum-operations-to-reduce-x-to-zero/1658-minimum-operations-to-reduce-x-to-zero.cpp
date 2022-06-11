a02f60ee2b4006a83ca358df91da4d6f394712d7
Zahid Khandaker Sumon <zksumon2017@gmail.com>
2022-06-12 00:51:17 +0600

    Time: 1179 ms (5.09%), Space: 212.8 MB (5.08%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -10,18 +10,19 @@ public:
             current = current + nums[i];
             p_sum[i] = current;
         }
-        map<int,int>ourmap;
+        unordered_map<int,int>ourmap;
         for(int i=0;i<p_sum.size();i++)ourmap[p_sum[i]]=i;
         ourmap[0] = p_sum.size();
         current = 0 ;
         int ans = 100000+1;
-        int temp,a,b;
+        int maptemp,temp,a,b;
         int ss = nums.size();
         for(int i=-1;i<ss;i++){
             if(i != -1)current = current + nums[i];
-            if(ourmap[x-current] != 0){
+            maptemp = ourmap[x-current];
+            if(maptemp != 0){
                 a = i;
-                b = ourmap[x-current];
+                b = maptemp;
                 if(b>a){
                     temp = a+1+p_sum.size()-b;
                     ans = min(ans,temp);
